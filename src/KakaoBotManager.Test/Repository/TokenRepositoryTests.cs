92ff16e1bb0aebac2f722059da60dec6cf38cd09
cola314 <ubj0314@naver.com>
2022-01-13 23:47:10 +0900

    test: TokenRepository 테스트 코드 추가

---
+++ src/KakaoBotManager.Test/Repository/TokenRepositoryTests.cs
@@ -0,0 +1,55 @@
+﻿using KakaoBotManager.Repository;
+using System;
+using Xunit;
+
+namespace KakaoBotManager.Test.Repository;
+
+public class TokenRepositoryTests
+{
+    private TokenRepository _repository;
+
+    public TokenRepositoryTests()
+    {
+        _repository = new TokenRepository();
+    }
+
+    [Fact]
+    public void CreateToken_WhenCalled_ReturnsNotEmptyToken()
+    {
+        var result = _repository.CreateToken();
+
+        Assert.True(Guid.Empty != result);
+    }
+
+    [Fact]
+    public void IsValidateToken_TokenIsLatestCreatedOne_ReturnsTrue()
+    {
+        var token = _repository.CreateToken();
+
+        var result = _repository.IsValidateToken(token);
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsValidateToken_AfterOtherTokenIsCreated_ReturnsFalse()
+    {
+        var token = _repository.CreateToken();
+        _repository.CreateToken();
+
+        var result = _repository.IsValidateToken(token);
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void IsValidateToken_InputIsExpiredToken_ReturnsFalse()
+    {
+        var token = _repository.CreateToken();
+        _repository.ExpireToken(token);
+
+        var result = _repository.IsValidateToken(token);
+
+        Assert.False(result);
+    }
+}
